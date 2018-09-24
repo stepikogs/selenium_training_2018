04521c6ca302cbbf7b25246cff75c38650dfe1b3
George Stepiko <stepikogs@gmail.com>
2018-09-23 20:07:48 -0700

    Stepiko - Task 3 - login scenario with no checks

--- test_example.py
+++ test_example.py
@@ -5,7 +5,10 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
+# temp structure to refactor!
 
+
+# fixtures
 @pytest.fixture
 def driver(request):
     wd = webdriver.Chrome()
@@ -19,3 +22,19 @@ def test_example(driver):
     driver.find_element_by_name("q").send_keys("webdriver")
     driver.find_element_by_name("btnK").click()
     WebDriverWait(driver, 10).until(ec.title_is("webdriver - Google Search"))
+
+
+def test_login(driver):   # no checks as while during task 3
+    driver.get('http://localhost/litecart/admin')
+    login(wd=driver)
+
+
+# service
+def login(wd, username="admin", password="secret"):
+    wd.find_element_by_name("username").click()
+    wd.find_element_by_name("username").clear()
+    wd.find_element_by_name("username").send_keys(username)
+    wd.find_element_by_name("password").click()
+    wd.find_element_by_name("password").clear()
+    wd.find_element_by_name("password").send_keys(password)
+    wd.find_element_by_name("login").click()
