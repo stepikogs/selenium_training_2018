8e1cf7bd9a8526554c6cb878dddfb34082099a76
George Stepiko <stepikogs@gmail.com>
2018-09-27 22:14:33 -0700

    Stepiko - Task 4 - run different browsers

--- test_example.py
+++ test_example.py
@@ -7,30 +7,50 @@ from selenium.webdriver.support import expected_conditions as ec
 
 # temp structure to refactor!
 
+browsers_to_test = (
+    'chrome',
+    'ie',
+    'firefox',
+    'jeegurda'   # just to check incorrect browser option
+)
 
-# fixtures
+
 @pytest.fixture
-def driver(request):
-    wd = webdriver.Chrome()
-    print(wd.capabilities)
-    request.addfinalizer(wd.quit)
-    return wd
+def my_driver(request):
+    def browser(br='chrome'):
+        if br.lower() == 'chrome':
+            wd = webdriver.Chrome()
+        elif br.lower() == 'firefox':
+            wd = webdriver.Firefox(capabilities={"marionette": False})
+        elif br.lower() == 'ie':
+            wd = webdriver.Ie()
+        else:
+            print('Incorrect browser requested. Running in Chrome instead.')
+            wd = webdriver.Chrome()
+        print(wd.capabilities)
+        request.addfinalizer(wd.quit)
+        return wd
+    return browser
 
 
-def test_example(driver):
+def test_example(my_driver):
+    driver = my_driver()
     driver.get("http://www.google.com/")
     driver.find_element_by_name("q").send_keys("webdriver")
     driver.find_element_by_name("btnK").click()
     WebDriverWait(driver, 10).until(ec.title_is("webdriver - Google Search"))
 
 
-def test_login(driver):   # no checks as while during task 3
+@pytest.mark.parametrize('brsr', browsers_to_test,
+                         ids=browsers_to_test)
+def test_login(my_driver, brsr):                               # no checks as while during task 3
+    driver = my_driver(brsr)
     driver.get('http://localhost/litecart/admin')
     login(wd=driver)
 
 
 # service
-def login(wd, username="admin", password="secret"):
+def login(wd, username="admin", password="admin"):
     wd.find_element_by_name("username").click()
     wd.find_element_by_name("username").clear()
     wd.find_element_by_name("username").send_keys(username)
