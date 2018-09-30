b28265c584d8999c0c2d8bcf004d7b21c10c0218
George Stepiko <stepikogs@gmail.com>
2018-09-29 23:45:42 -0700

    Stepiko - Task 8 - check stickers
    * minor updates for navigation
    + test_stickers

--- helpers.py
+++ helpers.py
@@ -1,5 +1,21 @@
 __author__ = 'George Stepiko'
 
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+
+HOME = 'http://localhost/litecart/'
+ODMEN = HOME + 'admin'
+
+
+def go_home(wd):
+    wd.get(HOME)
+    WebDriverWait(wd, 10).until(ec.title_is("Online Store | My Store"))
+
+
+def go_admin(wd):
+    wd.get(ODMEN)
+    WebDriverWait(wd, 10).until(ec.title_is("My Store"))
+
 
 def login(wd, username="admin", password="admin"):
     wd.find_element_by_name("username").click()
@@ -9,6 +25,11 @@ def login(wd, username="admin", password="admin"):
     wd.find_element_by_name("password").clear()
     wd.find_element_by_name("password").send_keys(password)
     wd.find_element_by_name("login").click()
+
+
+def admin_login(wd):
+    go_admin(wd)
+    login(wd)
 
 
 def get_items(context, locator):
--- test_search_elements.py
+++ test_search_elements.py
@@ -7,9 +7,7 @@ from selenium.webdriver.support import expected_conditions as ec
 
 def test_admin_headers(my_driver):
     driver = my_driver()
-    driver.get('http://localhost/litecart/admin')
-    WebDriverWait(driver, 10).until(ec.title_is("My Store"))
-    login(wd=driver)
+    admin_login(wd=driver)
     # get menu items as list
     main_menu = get_items(driver, '#app-')
     # iterate menu
@@ -25,3 +23,14 @@ def test_admin_headers(my_driver):
                 subber = get_items(driver, 'ul.docs li')[k]
                 subber.click()
                 assert driver.find_elements_by_css_selector('h1')
+
+
+def test_stickers(my_driver):
+    driver = my_driver()
+    go_home(driver)
+    products = get_items(driver, 'li.product.column.shadow.hover-light')
+    print('\ncheck stickers')
+    for item in products:
+        print('.', end='')
+        assert get_items(item, 'div.sticker')
+    print('\nfinished')
